670e26b335a7d12c3b65b995abd38c2a754741da
DARWICH ALI <adarwich@etu.unistra.fr>
2021-04-20 00:43:47 +0200

    m

---
+++ mpi_run.sh
@@ -0,0 +1,6 @@
+mpirun -n $1 ./checker input/a.txt out/a_best.out
+mpirun -n $1 ./checker input/b.txt out/b_best.out
+mpirun -n $1 ./checker input/c.txt out/c_best.out
+mpirun -n $1 ./checker input/d.txt out/d_best.out
+mpirun -n $1 ./checker input/e.txt out/e_best.out
+mpirun -n $1 ./checker input/f.txt out/f_best.out
